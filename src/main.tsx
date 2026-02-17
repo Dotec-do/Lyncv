b51451b974251a86c4fa5fc663e2864c97222c1b
dotec <plataformas@dotec.com>
2026-02-17 10:43:24 -0800

    init: scaffold Vite React TypeScript project

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./app.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <div>Lyncv</div>
+  </StrictMode>,
+);
